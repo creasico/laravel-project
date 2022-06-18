33af0ed16cbfcc35b99fffdc9009c2771ced608b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-18 09:28:30 +0700

    chore(lang): add indonesia locales
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Kredensial tersebut tidak sesuai dengan data kami.',
+    'password' => 'Password tersebut tidak sesuai.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+
+];
