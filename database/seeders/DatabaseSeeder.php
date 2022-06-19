fa4ebebd491bbd06914a78c1b5264f7bd44d7646
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-19 10:52:33 +0700

    chore(account): add metadata seeder & factory
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(MetadataSeeder::class);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
