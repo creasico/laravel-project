fc25902d373df5350a7ecd43a95aae2506132e8c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-09-03 05:45:58 +0700

    test(ci): disable seeder
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MetadataSeeder::class);
+        // $this->call(MetadataSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
