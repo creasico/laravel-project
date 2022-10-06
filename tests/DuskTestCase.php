c0dcd307bd318637cd95d94babfe638ffcd30184
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-10-07 06:45:26 +0700

    test(ci): don't start chrome driver on ci
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -26,7 +26,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
-        if (! static::runningInSail()) {
+        if (! static::runningInSail() || ! static::hasBrowserStackKey()) {
             static::startChromeDriver();
         }
 
