c8abf0776c233722a1dc7f8f15822f23b437d6cf
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-01-01 01:11:06 +0700

    chore: don't report command not found error
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -22,7 +23,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+        CommandNotFoundException::class,
     ];
 
     /**
