61c354c446a5b3869edb9ea80e86d9be1ea664d6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-01-01 01:06:47 +0700

    chore: add sentry to logging stack
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'sentry'],
             'ignore_exceptions' => false,
         ],
 
