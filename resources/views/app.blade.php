771e8928db5f39988ee7ff16a81b8bfeeabcc15c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-27 13:12:42 +0700

    chore(frontend): configure frontend for inertia-vue
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{-- <title>{{ config('app.name', 'Laravel') }} - @yield('title', 'Dashboard')</title> --}}
+
+        <link rel="icon" type="image/svg+xml" href="{{ asset('/assets/favicon.svg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('/assets/icon-192x192.png') }}">
+        <link rel="manifest" href="{{ asset('/build/manifest.webmanifest') }}" />
+
+        <!-- Scripts -->
+        @vite(['resources/client/app.ts', 'resources/client/app.css'])
+
+        @inertiaHead
+    </head>
+
+    <body class="font-sans antialiased min-h-screen w-full">
+        @inertia
+
+        <div id="dropdowns-wrapper"></div>
+    </body>
+</html>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { resolve } from 'node:path'
-import { defineConfig, loadEnv } from 'vite'
+import i18n from '@intlify/unplugin-vue-i18n/vite'
+import vue from '@vitejs/plugin-vue'
 import laravel from 'laravel-vite-plugin'
+import autoImport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import windicss from 'vite-plugin-windicss'
 import { VitePWA as pwa } from 'vite-plugin-pwa'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', ['APP', 'VITE'])
 
+  const rootdir = 'resources/client'
+
   return {
     resolve: {
       alias: {
-        '~/': `${resolve(__dirname, 'resources/client')}/`,
+        '~/': `${resolve(__dirname, rootdir)}/`,
+        'ziggy/': `${resolve('vendor/tightenco/ziggy/src/js')}/`,
       },
     },
 
@@ -18,6 +26,7 @@ export default defineConfig(({ mode }) => {
       'import.meta.env.APP_NAME': JSON.stringify(env.APP_NAME),
       'import.meta.env.APP_LOCALE': JSON.stringify(env.APP_LOCALE),
       'import.meta.env.APP_URL': JSON.stringify(env.APP_URL),
+      'import.meta.env.APP_ENV': JSON.stringify(env.APP_ENV),
     },
 
     plugins: [
@@ -26,15 +35,66 @@ export default defineConfig(({ mode }) => {
        */
       laravel({
         input: [
-          'resources/client/app.ts',
+          `${rootdir}/app.ts`,
         ],
-        refresh: true,
+        // refresh: true,
+      }),
+
+      vue({
+        include: [/\.vue$/],
+        template: {
+          // transformAssetUrl,
+        },
       }),
 
       /**
        * @see https://windicss.org/integrations/vite.html
        */
       windicss(),
+
+      /**
+       * @see https://github.com/antfu/unplugin-auto-import
+       */
+      autoImport({
+        dts: `${rootdir}/auto-imports.d.ts`,
+        dirs: [
+          `${rootdir}/composables`,
+          `${rootdir}/store`,
+        ],
+        imports: [
+          '@vueuse/core',
+          {
+            'naive-ui': [],
+          },
+          'vue-i18n',
+          'vue/macros',
+          'vue',
+        ],
+        vueTemplate: true,
+      }),
+
+      /**
+       * @see https://github.com/antfu/unplugin-vue-components
+       */
+      components({
+        dts: `${rootdir}/components.d.ts`,
+        dirs: [
+          `${rootdir}/components`,
+          `${rootdir}/layouts`,
+        ],
+        resolvers: [
+          NaiveUiResolver(),
+        ],
+      }),
+
+      /**
+       * @see https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+       */
+      i18n({
+        runtimeOnly: true,
+        compositionOnly: true,
+        include: [resolve(__dirname, './resources/lang/**')],
+      }),
 
       /**
        * @see https://vite-pwa-org.netlify.app/guide
@@ -54,7 +114,7 @@ export default defineConfig(({ mode }) => {
           id: '/',
           name: env.APP_NAME,
           short_name: env.APP_NAME,
-          start_url: env.APP_URL,
+          start_url: '/',
           display: 'fullscreen',
           background_color: '#ffffff',
           lang: env.APP_LOCALE || 'id',
