2d2a8b5561a5a316f6068c09e985ed1735e69982
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-29 05:29:48 +0700

    chore: update  envvar type definition and rename  to
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/client/env.d.ts
+++ resources/client/env.d.ts
@@ -9,14 +9,8 @@ interface ImportMetaEnv {
   APP_LOCALE?: 'id' | 'en';
   APP_URL?: string;
   VITE_API_URL?: string;
+  VITE_GTM_ID?: string;
   VITE_SENTRY_DSN?: string;
-  VITE_SENTRY_TRACES_SAMPLE_RATE?: number;
-  VITE_SENTRY_TRACE_QUEUE_ENABLED?: string;
-  // VITE_PUSHER_APP_KEY?: string;
-  // VITE_PUSHER_HOST?: string;
-  // VITE_PUSHER_PORT?: string;
-  // VITE_PUSHER_SCHEME?: string;
-  // VITE_PUSHER_APP_CLUSTER?: string;
 }
 
 declare module '*.vue' {
