2527272f64904057537bfc0f592f9dfb100a8477
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-30 05:22:01 +0700

    chore: ensure translation value is a string
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/client/modules/i18n.ts
+++ resources/client/modules/i18n.ts
@@ -2,10 +2,15 @@ import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
 export const install: AppModuleInstall = ({ app }): void => {
+  type Value = string | string[] | null
+
   for (const [locale, translations] of Object.entries(window.__translations)) {
     const trans: Record<string, any> = {}
 
-    for (const [key, value] of Object.entries<string>(translations)) {
+    for (const [key, value] of Object.entries<Value>(translations)) {
+      if (typeof value !== 'string')
+        continue
+
       trans[key] = value.includes(':')
         ? value.replace(/:(\w+)/g, (_: string, ...args: any[]) => `{${args[0].toLowerCase()}}`)
         : value
