d9d7bab9d3ccf60a7e1ffae36a5661d7b7ab9857
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-07-01 02:03:25 +0700

    feat: initialize user management pages
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        {{ __('dashboard.routes.index') }}
-    </x-slot>
-
-    <x-card class="p-6">
-        <h3 class="">{{ __('dashboard.welcome-notice', ['user' => Auth::user()->name]) }}</h3>
-    </x-card>
-</x-app-layout>
--- resources/views/users/form.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        {{ __('users.routes.index') }}
-    </x-slot>
-
-    <x-card class="p-6">
-        <h3 class="">User form goes here</h3>
-    </x-card>
-</x-app-layout>
