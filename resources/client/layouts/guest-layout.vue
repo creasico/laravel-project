39aa7b421df74e8fa3b7cb9e2607e87396bdc579
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-30 07:02:55 +0700

    chore(auth): initilize guest layout for unauthenticated pages
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ resources/client/layouts/guest-layout.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+
+const { offlineReady } = useRegisterSW({
+  immediate: true,
+})
+
+onMounted(async () => {
+  const { registerSW } = await import('virtual:pwa-register')
+
+  if (offlineReady.value)
+    console.log('Offline ready', 'Your app is offline ready') // eslint-disable-line no-console
+
+  registerSW({
+    immediate: true,
+  })
+})
+</script>
+
+<template>
+  <n-space vertical size="large">
+    <n-layout>
+      <n-layout-content content-style="padding: 12px">
+        <slot />
+      </n-layout-content>
+    </n-layout>
+  </n-space>
+</template>
+
+<style lang="postcss">
+/*  */
+</style>
