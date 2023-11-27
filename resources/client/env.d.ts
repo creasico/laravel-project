049780670e23df8ccacecb0e4df5ce50b1e38b8a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-11-28 04:30:10 +0700

    chore: update required environment variables
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/client/env.d.ts
+++ resources/client/env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   APP_NAME?: string;
   APP_LOCALE?: AppLocale;
   APP_URL?: string;
+  FIREBASE_VAPID_KEY?: string;
   VITE_API_URL?: string;
   VITE_GTM_ID?: string;
   SENTRY_DSN?: string;
