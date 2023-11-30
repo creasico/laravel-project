dccf79c73c0606f7c08a72ace7db474b8542b735
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-12-01 05:05:09 +0700

    chore: switch service worker strategy to network-first
    see: https://stackoverflow.com/a/48325023/881743
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/client/sw.ts
+++ resources/client/sw.ts
@@ -3,14 +3,14 @@ import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw'
 import { cacheNames, clientsClaim } from 'workbox-core'
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 import { setDefaultHandler } from 'workbox-routing'
-import { CacheFirst } from 'workbox-strategies'
+import { NetworkFirst } from 'workbox-strategies'
 
 declare let self: ServiceWorkerGlobalScope & Record<string, any>
 const cacheName = cacheNames.runtime
 
 cleanupOutdatedCaches()
 
-setDefaultHandler(new CacheFirst({ cacheName }))
+setDefaultHandler(new NetworkFirst({ cacheName }))
 
 precacheAndRoute(self.__WB_MANIFEST)
 
